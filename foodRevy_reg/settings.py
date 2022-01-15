154a71e31ce37d3e4cc9f28e543f3ef0e0d50af6
Ade-yem <93265896+Ade-yem@users.noreply.github.com>
2022-01-15 15:55:08 +0100

    form

--- foodRevy_reg/settings.py
+++ foodRevy_reg/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-v51-30==*g+wb%2ne6ch_)zlmy0odga5+muygc=l18d%wkps92'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
